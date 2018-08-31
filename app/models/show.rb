a721626240530d3a4c75e13842f969fb9798c790
Leslie Stevens <flylady@mail.utexas.edu>
2018-08-31 19:01:19 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,7 +5,7 @@ class Show < ActiveRecord::Base
   end
 
   def self.most_popular_show
-    show = Show.where("rating = Show.maximum(:rating)")
+    show = Show.rating = self.highest_rating
     show
   end
 
